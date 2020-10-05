6a7df109daaba46820efea7c560b992c8dba2d8e
codacy20 <kmuc72@gmail.com>
2020-10-05 23:28:55 +0200

    image container paragraph styling

--- src/library/common/Image-Container/ImageContainer.jsx
+++ src/library/common/Image-Container/ImageContainer.jsx
@@ -4,12 +4,14 @@ import './ImageContainer.scss';
 function ImageContainer(props) {
   return (
     <div className="image-container">
-      <p id="wid-50">
+      <p className="wid-50">“</p>
+      <p className="wid-50">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vulputate
         eget mi non aliquam. Suspendisse id efficitur lorem. Vivamus facilisis
         fringilla leo, et blandit arcu mattis porttitor. Phasellus consequat
         augue.
       </p>
+      <p className="wid-50">⅃</p>
     </div>
   );
 }
