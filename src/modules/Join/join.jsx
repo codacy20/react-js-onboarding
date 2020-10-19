80d36e7078424d037f94e12b42d3e655fa0b71de
codacy20 <kmuc72@gmail.com>
2020-10-19 23:24:33 +0200

    join us styling and navigation

--- src/modules/Join/join.jsx
+++ src/modules/Join/join.jsx
@@ -5,7 +5,10 @@ import './join.scss';
 import '../../library/common/common.scss';
 
 function Join(props) {
-  
+  function navigate() {
+    window.location.href = '/register';
+  }
+
   return (
     <div className="container join-container">
       <div className="left">
@@ -13,7 +16,33 @@ function Join(props) {
       </div>
       <div className="right">
         <Stepper back={{ visible: false }} step={{ visible: false }} />
-        <span>join</span>
+        <div className="join-inner-container">
+          <h2>Join Us!</h2>
+          <p>
+            To begin this journey, tell us what type of account you’d be
+            opening.
+          </p>
+          <button onClick={() => navigate()}>
+            <div className="pentagon">
+              <span className="material-icons">person_outline</span>{' '}
+            </div>
+            <div className="txt">
+              <h3>Individual</h3>
+              <p>Personal account to manage all you activities.</p>
+            </div>
+            <span className="material-icons">arrow_forward</span>
+          </button>
+          <button onClick={() => navigate()}>
+            <div className="pentagon">
+              <span className="material-icons">work_outline</span>
+            </div>
+            <div className="txt">
+              <h3>Business</h3>
+              <p>Own or belong to a company, this is for you.</p>
+            </div>
+            <span className="material-icons">arrow_forward</span>
+          </button>
+        </div>
       </div>
     </div>
   );
